03ffe87f4a8558297202db8d79c49951a0235fbc
manu <manu@localhost.localdomain>
2019-03-22 14:17:02 +0530

    HTML Preview Added

--- client/main.js
+++ client/main.js
@@ -5,6 +5,30 @@ import './main.html';
 
 Template.editor.helpers({
   docid:function(){
-    return Documents.findOne()._id;
+    var doc = Documents.findOne();
+    if(doc){
+    return doc._id;
+    }
+    else{
+      return undefined;
+    }
+  },
+  config:function(){
+    return function(editor){
+      editor.on("change",function(cm_editor, info){
+        console.log(cm_editor.getValue());
+        $("#iframe-viewer").contents().find("html").html(cm_editor.getValue());
+      });
+    }
   }
+});
+Template.docList.helpers({
+  document:function(){
+  doc = Documents.findOne();
+  if (doc){
+    return {"title":"nothing to see here"};
+  } else {
+    return doc;
+  }
+}
 });
